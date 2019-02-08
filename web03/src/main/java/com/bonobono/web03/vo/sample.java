ba1619847d93ee97daa1d0005f5812f74bf77893
Administrator <Administrator@smart000>
2019-02-08 10:55:29 +0900

    [문효균]sample.java 생성 -sample DTO 생성

---
+++ web03/src/main/java/com/bonobono/web03/vo/sample.java
@@ -0,0 +1,26 @@
+package com.bonobono.web03.vo;
+
+public class sample {
+	private int sampleId; // sample테이블 sample_id컬럼
+	private String sampleName; // sample_name
+	
+	
+	public int getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(int sampleId) {
+		this.sampleId = sampleId;
+	}
+	public String getSampleName() {
+		return sampleName;
+	}
+	public void setSampleName(String sampleName) {
+		this.sampleName = sampleName;
+	}
+	
+	@Override
+	public String toString() {
+		return "sample [sampleId=" + sampleId + ", sampleName=" + sampleName + "]";
+	}
+
+}
